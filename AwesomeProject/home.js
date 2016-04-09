0a8dcd8a1d3efdb0d32a32cb564635ca4920d5cf
Mr_GaoMing <184724949@qq.com>
2016-04-09 15:37:34 +0800

    RN
    RN

---
+++ AwesomeProject/home.js
@@ -0,0 +1,17 @@
+import React,{
+  StyleSheet,
+  View,
+  Text,
+  Component
+} from 'react-native';
+
+export default class Home extends Component {
+  constructor() {
+    super();
+  }
+  render(){
+    return(
+      <Text>HOME</Text>
+    );
+  }
+}
